97784f19739cfeb5d1a4081417a8ea1c57be9bf2
Mahady-Rayhan <mahadyr@gmail.com>
2020-03-24 13:06:55 -0400

    updated

--- src/com/syntax/class15/Main.java
+++ src/com/syntax/class15/Main.java
@@ -2,12 +2,12 @@ package com.syntax.class15;
 
 public class Main {
 	 String censorLetter(String censorLetter1, char charecter){
-		 censorLetter1 =censorLetter1.replace('e', '*');
+		 censorLetter1 =censorLetter1.replace(charecter, '*');
 		    return censorLetter1;
 		  }
 	 public static void main(String[] args) {
 		Main obj=new Main();
-		String objj=obj.censorLetter("computer", ' ');
+		String objj=obj.censorLetter("computer", 'e');
 		System.out.println(objj);
 	}
 }
